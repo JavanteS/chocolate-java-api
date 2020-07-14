e55594ded9e14d35d5003df1a019e7a3003f5e9a
javante_nelson <javantenelson@gmail.com>
2020-07-14 00:15:56 -0500

    add another recipe and its category to api

--- chocolate-backend/db/seeds.rb
+++ chocolate-backend/db/seeds.rb
@@ -7,3 +7,4 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 recipe_two = (title: "Gooey Chocolate Brownies", instructions: "1. Mix 3 cups of flour, 1tsp of baking soda, 1/2tsp of salt to in a bowl. Set aside. /n/Melt 1/3 cup of butter", difficulty_level: "Beginner", img_url: "https://images.pexels.com/photos/2067396/pexels-photo-2067396.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260")
+recipe_three = (title: "Chocolate Cake with rasphberry filling", instructions: "1. Sift 3 cups of flour, 1tsp of baking soda, 1/2tsp of salt to in a bowl. Set aside. /n/Melt 1/3 cup of butter and pour a cup of white sugar.", difficulty_level: "Intermediate", img_url: "https://images.pexels.com/photos/3252137/pexels-photo-3252137.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
