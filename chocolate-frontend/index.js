9ed742a33b13b42678eb74c60adb722a4b6f3618
javante_nelson <javantenelson@gmail.com>
2020-07-14 17:08:15 -0500

    add id to div class get object from api

--- chocolate-frontend/index.js
+++ chocolate-frontend/index.js
@@ -20,10 +20,12 @@ function displayChocolates(){
 function displaychocolate(coco){
     let div = document.createElement("div")
     let container = document.getElementById("chocolates-container")
-    div.innerHTML += `<div class="chocolate-card">
-    <h2>${coco.title}</h2>
+    div.innerHTML += `<div class="chocolate-card" id="${coco.id}">
     <img src="${coco.img_url}"/>
+    <div class="container">
+    <h2>${coco.title}</h2>
     <p>${coco.difficulty_level}</p>
+     </div>
     </div>`
 
     container.appendChild(div)
@@ -33,20 +35,26 @@ function flipCard(){
     let cards = document.querySelectorAll(".chocolate-card")
     cards.forEach(card=>{
         card.addEventListener("click", function(event){
-            showcard(card)
+            showcard(card.querySelector("img").dataset.id)
         })
         
     })
 }
 
 function showcard(card){
-
     
+    fetch(BASE_URL+`/recipes/${card.id}`)
+    .then(resp => resp.json)
 
-
+    .then(data => {
+        displayone(data)
+    })
 
 }
 
+function display(data)
+
 document.addEventListener("DOMContentLoaded", ()=>{
     displayChocolates()
+    flipCard()
 })
