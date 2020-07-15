8c6ca56a158fd0e1e513e315816ccae73c745a8a
javante_nelson <javantenelson@gmail.com>
2020-07-15 00:14:33 -0500

    will cakes to show on page

--- chocolate-frontend/index.js
+++ chocolate-frontend/index.js
@@ -2,7 +2,7 @@ const BASE_URL = "http://localhost:3000"
 
 
 
-function displayChocolates(){
+function fetchChocolates(){
 
     let container = document.getElementById("chocolates-container")
     return fetch(BASE_URL+"/recipes")
@@ -11,13 +11,14 @@ function displayChocolates(){
 
     .then(json => {
         json.forEach(coco =>{
-            displaychocolate(coco)
+            displayChocolates(coco)
+            make_clickable()
         })
    
     })
 }
 
-function displaychocolate(coco){
+function displayChocolates(coco){
     let div = document.createElement("div")
     let container = document.getElementById("chocolates-container")
     div.innerHTML += `<div class="chocolate-card" id="${coco.id}">
@@ -31,30 +32,79 @@ function displaychocolate(coco){
     container.appendChild(div)
 }
 
-function flipCard(){
-    let cards = document.querySelectorAll(".chocolate-card")
-    cards.forEach(card=>{
-        card.addEventListener("click", function(event){
-            showcard(card.querySelector("img").dataset.id)
-        })
-        
-    })
-}
 
-function showcard(card){
+
+function showCard(id){
     
-    fetch(BASE_URL+`/recipes/${card.id}`)
+   return fetch(BASE_URL+`/recipes/${id}`)
     .then(resp => resp.json)
 
     .then(data => {
-        displayone(data)
+        displayOne(data)
     })
 
 }
 
-function display(data)
+function displayOne(data){
+    let container = document.getElementById("chocolates-container")
+    container.innerHTML = ""
+    let div = document.createElement("div")
+    div.innerHTML = `<div class="info-container" id="${data.id}>
+    <p>${data.title}</p>
+    <p>${data.difficulty_level}</p>
+    <p>${data.instructions}</p>
+    </div>`
+
+    container.appendChild(div)
+
+    
+}
+
+function fetchcategories(){
+    // let cakeId = document.getElementById("cakes")
+    // let chocolates = Array.from(document.querySelectorAll(".chocolate-card").children)
+
+    fetch(BASE_URL+"/categories")
+    .then(resp => resp.json())
+    .then(data => displayCategories(data))
+    // cake.addEventListener("click", event=> {
+    //     cake = chocolates.filter
+
+    // }
+    // )
+}
+
+
+function make_clickable(){
+    let cakeId = document.getElementById("cakes")
+
+}
+
+function displayCategories(data){
+    let cakeId = document.getElementById("cakes")
+    let container = document.getElementById("chocolates-container")
+      cakeId.addEventListener("click", displayCakes)
+
+}
+
+
 
 document.addEventListener("DOMContentLoaded", ()=>{
-    displayChocolates()
-    flipCard()
+    fetchChocolates()
+    fetchcategories()
+    
+    
+    function flipCard(){
+        let cards = document.querySelectorAll(".chocolate-card")
+        cards.forEach(card=>{
+            card.addEventListener("click", function(event){
+                showCard(card.querySelector("img").dataset.id)
+            })
+            
+        })
+    }
+    
+    
+   
+    
 })
